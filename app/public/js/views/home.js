bf823edd7c3a05bc48586ee30bea75f286d6cd0c
tommia87 <tommia87@gmail.com>
2014-06-05 00:55:18 +0000

    added/changed various files for heroku

--- app/public/js/views/home.js
+++ app/public/js/views/home.js
@@ -36,6 +36,9 @@ $(document).ready(function(){
 	$('#account-form-btn1').html('Delete');
 	$('#account-form-btn1').addClass('btn-danger');
 	$('#account-form-btn2').html('Update');
+	$('#account-form h2').text('My Guesses');
+	$('#account-form #sub2').text('Here are your current guesses.');
+
 
 // setup the confirm window that displays when the user chooses to delete their account //
 
